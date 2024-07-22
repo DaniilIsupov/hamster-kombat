6eb472e8dcaab710d47fecc087eb0ffcfed0eb6c
DaniilIsupov <disupov1996@gmail.com>
2024-07-22 08:52:29 +0200

    fix: oops (d8e61affae3c1692b571d8ccafc6923f5376e5ff)

--- postCheckTask.js
+++
@@ -1,7 +0,0 @@
-await useNuxtApp()
-    .$pinia._s.get('earn')
-    .postCheckTask(
-        await useNuxtApp()
-            .$pinia._s.get('earn')
-            .tasks.filter((t) => !t.isCompleted)[0].id
-    );
